b3384ec18ea2d21dcf96bb25cc0ce88f41ee5475
Irfan Danish <irfan@limabean.co.za>
2025-01-09 03:41:29 +0100

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Faheem Riaz on 09.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
